136ed22fa88b3ba9b42b16527ddfc8222f2af91b
hkonate <konate.tronca@gmail.com>
2022-07-28 03:41:28 +0200

    offers pages done

--- src/pages/Offer.js
+++ src/pages/Offer.js
@@ -12,49 +12,64 @@ const Offer = ({ data, setData }) => {
                             <img key={index} src={user.product_image.secure_url} alt="product-pic" />
                         </div>
                         <div className='right'>
-                            <div>
+                            <div className='product-details'>
+                                <p>{user.product_price} €</p>
                                 {user.product_details.map((product, index) => {
-                                    return (<ul key={index}>
-                                        {product.MARQUE &&
-                                            <li>
-                                                <span>Marque</span>
-                                                <span>{product.MARQUE}</span>
-                                            </li>
-                                        }
+                                    return (
+                                        <ul key={index}>
+                                            {product.MARQUE &&
+                                                <li>
+                                                    <span>Marque</span>
+                                                    <span>{product.MARQUE}</span>
+                                                </li>
+                                            }
 
-                                        {product.TAILLE &&
-                                            <li>
-                                                <span>TAILLE</span>
-                                                <span>{product.TAILLE}</span>
-                                            </li>
-                                        }
-                                        {product.ETAT &&
-                                            <li>
-                                                <span>ETAT</span>
-                                                <span>{product.ETAT}</span>
-                                            </li>
-                                        }
-                                        {product.COULEUR &&
-                                            <li>
-                                                <span>COULEUR</span>
-                                                <span>{product.COULEUR}</span>
-                                            </li>
-                                        }
-                                        {product.EMPLACEMENT &&
-                                            <li>
-                                                <span>EMPLACEMENT</span>
-                                                <span>{product.EMPLACEMENT}</span>
-                                            </li>
-                                        }
-                                    </ul>)
+                                            {product.TAILLE &&
+                                                <li>
+                                                    <span>TAILLE</span>
+                                                    <span>{product.TAILLE}</span>
+                                                </li>
+                                            }
+                                            {product.ÉTAT &&
+                                                <li>
+                                                    <span>ETAT</span>
+                                                    <span>{product.ÉTAT}</span>
+                                                </li>
+                                            }
+                                            {product.COULEUR &&
+                                                <li>
+                                                    <span>COULEUR</span>
+                                                    <span>{product.COULEUR}</span>
+                                                </li>
+                                            }
+                                            {product.EMPLACEMENT &&
+                                                <li>
+                                                    <span>EMPLACEMENT</span>
+                                                    <span>{product.EMPLACEMENT}</span>
+                                                </li>
+                                            }
+                                        </ul>)
                                 })
                                 }
                             </div>
-                            <div></div>
-                            <div></div>
+                            <div className='line'></div>
+                            <div className='offer-descp'>
+                                {user.product_name && <p>{user.product_name}</p>}
+                                {user.product_description && <p>{user.product_description}</p>}
+                                {user.owner &&
+                                    <div className='offer-avatar'>
+                                        <img src={user.owner.account.avatar.secure_url} alt="" />
+                                        <p>{user.owner.account.username}</p>
+                                    </div>}
+
+                            </div>
+
                             {/* <Link to="/">Home</Link> */}
+                            <button>Acheter</button>
                         </div>
+
                     </div>
+
                 </div>)
         }))
 }
